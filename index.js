1bd99b6df092cb34edc09574e2e86886f437a1f1
michael <michaelyoussef396@gmail.com>
2023-10-01 12:25:14 +1100

    finished

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    dodger.style.left = `${left - 1}px`;
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    dodger.style.left = `${left + 1}px`;
+}
